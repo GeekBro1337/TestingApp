c59dbf79325be6cdf62c44b7e865b7df1bd431ee
GeekBro1337 <kenesov_alisher@mail.ru>
2025-06-29 21:22:46 +0500

    A constructor for creating new tests has been made

--- types/tests.d.ts
+++ types/tests.d.ts
@@ -1,45 +1,44 @@
-
-export {}
+export {};
 
 declare global {
-        namespace Test {
-            interface Metadata{
-                fileName: string,
-                title: string,
-                fields: FormField[]
-                // answers: answer[]
-            }
+  namespace Test {
+    interface Metadata {
+      fileName: string;
+      title: string;
+      fields: FormField[];
+      // answers: answer[]
+    }
 
-            // interface answer {
-            //     key: uuid,
-            //     answer: string
-            // }
+    // interface answer {
+    //     key: uuid,
+    //     answer: string
+    // }
 
-        interface FormField {
-            id: number 
-            name: string
-            label: string
-            type: 'text'| 'number' | 'textarea' | 'checkboxMultiple'
-            placeholder?: string
-            required: boolean
-            validation?: { 
-                minLength?: number
-                maxLength?: number
-                pattern?: RegExp
-                min?: number
-                max?: number
-                step?: number
-            }
-            options?: Array<{label: string; value: string}>
-        }
-        interface FormConfig { 
-            title: string
-            description: string
-            fields: FormField[]
-            metadata?: Metadata
-        }
-        interface FormState {
-            [key: string]: string | number | boolean | string[] | undefined
-        }
-        }
+    interface FormField {
+      id: number;
+      name: string;
+      label: string;
+      type: "text" | "number" | "textarea" | "checkboxGroup";
+      placeholder?: string;
+      required: boolean;
+      validation?: {
+        minLength?: number;
+        maxLength?: number;
+        pattern?: RegExp;
+        min?: number;
+        max?: number;
+        step?: number;
+      };
+      options?: Array<{ label: string; value: string }>;
+    }
+    interface FormConfig {
+      title: string;
+      description: string;
+      fields: FormField[];
+      metadata?: Metadata;
+    }
+    interface FormState {
+      [key: string]: string | number | boolean | string[] | undefined;
+    }
+  }
 }
